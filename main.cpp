14ddee8a0a76d915029b23942cd8d864a9ab2c06
JackBillingham <billingham.jacks@gmail.com>
2025-02-22 22:16:07 +0100

    GenerateRoom

--- main.cpp
+++ main.cpp
@@ -5,5 +5,24 @@
 #include <ctime>
 
 using namespace std;
+class Dungeon {
+public:
+Dungeon(int width, int height) : width(width), height(height) {
+    grid.resize(height, vector<string>(width, "wall"));
+}
 
-class Dungeon
+void generateRooms(int numRooms) {
+    for (int i = 0; i < numRooms; i++) {
+        int roomWidth = randInt(5, 10);
+        int roomHeight = randInt(5, 10);
+        int roomX = randInt(1, width - roomWidth - 1);
+        int roomY = randInt(1, height - roomHeight - 1);
+
+        if (checkCollision(roomX, roomY, roomWidth, roomHeight)) {
+            continue;
+        }
+
+        placeRoom(roomX, roomY, roomWidth, roomHeight);
+        rooms.push_back({roomX, roomY, roomWidth, roomHeight});
+    }
+}
