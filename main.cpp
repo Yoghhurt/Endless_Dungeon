39fdd56b7d2f0c2a8a3b99c919c7e511055786d2
JackBillingham <billingham.jacks@gmail.com>
2025-02-24 18:32:48 +0100

    Rewrote the code, with some comments

--- main.cpp
+++ main.cpp
@@ -1,60 +1,104 @@
 #include <iostream>
 #include <vector>
-#include <random>
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
+#define WIDTH 20
+#define HEIGHT 20
+
+enum Tile { Empty = '.', Wall = '#', Start = 'S', End = 'E' };
+
 class Dungeon {
 public:
-Dungeon(int width, int height) : width(width), height(height) {
-    grid.resize(height, vector<string>(width, "wall"));
-}
+    Dungeon() {
+        // Random number generator
+        std::srand(std::time(0));
 
-void generateRooms(int numRooms) {
-    for (int i = 0; i < numRooms; i++) {
-        int roomWidth = randInt(5, 10);
-        int roomHeight = randInt(5, 10);
-        int roomX = randInt(1, width - roomWidth - 1);
-        int roomY = randInt(1, height - roomHeight - 1);
+        // Initialize the dungeon with walls
+        dungeonGrid = std::vector<std::vector<Tile>>(HEIGHT, std::vector<Tile>(WIDTH, Wall));
+    }
 
-        if (checkCollision(roomX, roomY, roomWidth, roomHeight)) {
-            continue;
+    void generate() {
+        // Clear the grid and reset
+        for (int i = 0; i < HEIGHT; ++i) {
+            for (int j = 0; j < WIDTH; ++j) {
+                dungeonGrid[i][j] = Wall;
+            }
         }
 
-        placeRoom(roomX, roomY, roomWidth, roomHeight);
-        rooms.push_back({roomX, roomY, roomWidth, roomHeight});
-    }
-}
+        // Create random rooms
+        createRandomRooms();
 
-private:
-    int width, height;
-    vector<vector<string>> grid;
-    vector<tuple<int, int, int, int>> rooms;  // (x, y, width, height)
-    pair<int, int> start, end;
+        // Create corridors connecting rooms
+        createCorridors();
 
-    int randInt(int min, int max) {
-        static random_device rd;
-        static mt19937 gen(rd());
-        uniform_int_distribution<> dis(min, max);
-        return dis(gen);
+        // Place the start (S) and end (E) points
+        placeStartAndEnd();
     }
 
-    bool checkCollision(int x, int y, int width, int height) {
-        for (int i = x; i < x + width; i++) {
-            for (int j = y; j < y + height; j++) {
-                if (grid[j][i] != "wall") {
-                    return true;
+    void printDungeon() {
+        for (int i = 0; i < HEIGHT; ++i) {
+            for (int j = 0; j < WIDTH; ++j) {
+                std::cout << (char)dungeonGrid[i][j];
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    private:
+    std::vector<std::vector<Tile>> dungeonGrid;
+
+    void createRandomRooms() {
+        // Randomly generate rooms
+        for (int i = 0; i < 5; ++i) {
+            int roomWidth = std::rand() % 4 + 4;   // Room width between 4 and 7
+            int roomHeight = std::rand() % 4 + 4;  // Room height between 4 and 7
+            int roomX = std::rand() % (WIDTH - roomWidth - 1);
+            int roomY = std::rand() % (HEIGHT - roomHeight - 1);
+
+            // Fill the room area with empty spaces
+            for (int y = roomY; y < roomY + roomHeight; ++y) {
+                for (int x = roomX; x < roomX + roomWidth; ++x) {
+                    dungeonGrid[y][x] = Empty;
                 }
             }
         }
-        return false;
     }
 
-    void placeRoom(int x, int y, int width, int height) {
-        for (int i = x; i < x + width; i++) {
-            for (int j = y; j < y + height; j++) {
-                grid[j][i] = "room";
+    void createCorridors() {
+        for (int y = 1; y < HEIGHT; ++y) {
+            for (int x = 1; x < WIDTH; ++x) {
+                if (dungeonGrid[y][x] == Empty) {
+                    if (y > 0 && dungeonGrid[y - 1][x] == Wall) {
+                        dungeonGrid[y - 1][x] = Empty; // Vertical corridor
+                    }
+                    if (x > 0 && dungeonGrid[y][x - 1] == Wall) {
+                        dungeonGrid[y][x - 1] = Empty; // Horizontal corridor
+                    }
+                }
             }
         }
     }
+
+    void placeStartAndEnd() {
+        int startX = std::rand() % WIDTH;
+        int startY = std::rand() % HEIGHT;
+        int endX = std::rand() % WIDTH;
+        int endY = std::rand() % HEIGHT;
+
+      
+        while (dungeonGrid[startY][startX] != Empty) {
+            startX = std::rand() % WIDTH;
+            startY = std::rand() % HEIGHT;
+        }
+
+        while (dungeonGrid[endY][endX] != Empty || (startX == endX && startY == endY)) {
+            endX = std::rand() % WIDTH;
+            endY = std::rand() % HEIGHT;
+        }
+
+        // Place start and end
+        dungeonGrid[startY][startX] = Start;
+        dungeonGrid[endY][endX] = End;
+    }
+};
